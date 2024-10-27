2462bb998d2b0379d0cee0689bd53785f2e1c277
vdomash <viktor.domashnev@gmail.com>
2024-10-27 00:04:01 -0400

    Task 2.1

--- src/main/java/use_case/login/LoginUserDataAccessInterface.java
+++ src/main/java/use_case/login/LoginUserDataAccessInterface.java
@@ -27,4 +27,9 @@ public interface LoginUserDataAccessInterface {
      */
     User get(String username);
 
+    /**
+     * Caches the current user.
+     * @param name the username to cache
+     */
+    void setCurrentUser(String name);
 }
